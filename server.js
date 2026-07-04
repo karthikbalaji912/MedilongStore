535b12eb1c05c430184414386db3d2605f1e1da8
KARTHIK BALAJI R <karthikbalaji912@gmail.com>
2026-07-04 15:56:57 +0530

    Serve login.html on root route

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
-
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+});
 const JWT_SECRET = "KAMALESH"; // Remember to use an environment variable in production
 
 // ======================== MONGOOSE DATABASE CONNECTION ========================
